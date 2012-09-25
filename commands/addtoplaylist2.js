5971241065827e5644e7a51a6d87fe97dbfafe6f
Mike Wills <mike@mikewills.me>
2012-09-24 23:30:34 -0500

    Another addsong command

---
+++ commands/addtoplaylist2.js
@@ -0,0 +1,15 @@
+exports.name = 'addsong';
+exports.hidden = true;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data, userid, source) {
+    IsMod(userid, function(isMod) {
+        if (isMod) {
+            bot.playlistAll(function(playlist) {
+                bot.playlistAdd(currentsong.id, playlist.list.length);
+            });
+            bot.snag();
+            bot.vote('up');
+        }
+    });
+}
