3187ba2e2503dbd7635dd290a059094ff5a68ed6
Mike Wills <mike@mikewills.me>
2012-09-24 22:10:03 -0500

    Added another hit bot.

---
+++ commands/hitbot2.js
@@ -0,0 +1,8 @@
+exports.name = '/me punches @' + botName.toLowerCase();
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data, userid, source) {
+	SpeakRandom(hitBot, AllUsers[userid].name);
+	bot.vote('up');
+}
