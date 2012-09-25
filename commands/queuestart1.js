8c54b7bcde3e07c32d8641f3797968f4e99d93e4
Mike Wills <mike@mikewills.me>
2012-09-24 22:40:32 -0500

    Added shell of start and end queue

---
+++ commands/queuestart1.js
@@ -0,0 +1,7 @@
+exports.name = '@' + botName.toLowerCase() + ' startq';
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data, userid, source) {
+	
+}
