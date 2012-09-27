7f4dfd6be7befdc21f7a36efbe6f4c476c792c71
Mike Wills <mike@mikewills.me>
2012-09-27 00:02:37 -0500

    Oops… not set to mod only.

--- commands/ban.js
+++ commands/ban.js
@@ -3,13 +3,17 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data, userid, source) {
-	var userName = data.text.substring(6);
+	IsMod(userid, function(isMod) {
+		if (isMod) {
+			var userName = data.text.substring(6);
 
-	for (i in AllUsers) {
-		if (AllUsers[i].name == userName) {
-			Log(color("EVENT BOOT: ", "red") + AllUsers[i].name + " - " + i);
-			client.query("INSERT INTO BANNED (`userid`, `username`, `timestamp`) VALUES (?, ?, CURRENT_TIMESTAMP)", [i, userName]);
-			bot.bootUser(i, "");
+			for (i in AllUsers) {
+				if (AllUsers[i].name == userName) {
+					Log(color("EVENT BOOT: ", "red") + AllUsers[i].name + " - " + i);
+					client.query("INSERT INTO BANNED (`userid`, `username`, `timestamp`) VALUES (?, ?, CURRENT_TIMESTAMP)", [i, userName]);
+					bot.bootUser(i, "");
+				}
+			}
 		}
-	}
+	});
 }
