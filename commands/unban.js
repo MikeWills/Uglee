dd541ae57495f2ebd9045f63bbae9da9d99fab7a
Mike Wills <mike@mikewills.me>
2012-09-27 22:02:29 -0500

    Can now unban a user.

--- commands/unban.js
+++ commands/unban.js
@@ -1,7 +1,16 @@
-exports.name = 'unban';
+exports.name = '/unban';
 exports.hidden = false;
 exports.enabled = true;
-exports.matchStart = false;
+exports.matchStart = true;
 exports.handler = function(data, userid, source) {
-	// remove banned user from database
+	IsMod(userid, function(isMod) {
+		if (isMod) {
+			var userName = data.text.substring(8);
+			Log(userName);
+
+			client.query("DELETE FROM BANNED WHERE `username` = ?", [userName]);
+			Speak("@" + userName + " is no longer banned.");
+			Log("Unbanned " + userName);
+		}
+	});
 }
