f2d2060f6c902f4dcb212dc5c715f1cd0b13063c
Mike Wills <mike@mikewills.me>
2012-10-01 22:32:14 -0500

    Renamed /banuser to /ban

--- commands/ban.js
+++ commands/ban.js
@@ -1,19 +1,24 @@
 exports.name = '/ban';
-exports.hidden = false;
+exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data, userid, source) {
-	IsMod(userid, function(isMod) {
-		if (isMod) {
-			var userName = data.text.substring(6);
+    IsMod(userid, function(isMod) {
+        if (isMod) {
+            //Get name and userid
+            var givenname = data.text.substring(9);
+            client.query('SELECT userid FROM (SELECT * FROM ' + dbName + '.' + dbTablePrefix + 'User WHERE username LIKE ?) a ORDER BY lastseen DESC', [givenname], function select(error, results, fields) {
+                if (results != null && results.length > 0) {
+                    addToBanList(results[0]['userid'], givenname, data.name);
+                }
+            });
+        }
+    });
+}
 
-			for (i in AllUsers) {
-				if (AllUsers[i].name == userName) {
-					Log(color("EVENT BOOT: ", "red") + AllUsers[i].name + " - " + i);
-					client.query("INSERT INTO BANNED (`userid`, `username`, `timestamp`) VALUES (?, ?, CURRENT_TIMESTAMP)", [i, userName]);
-					bot.bootUser(i, "");
-				}
-			}
-		}
-	});
+function addToBanList(userid, name, bannedby) {
+    client.query('INSERT INTO ' + dbName + '.BANNED SET userid = ?, banned_by = ?, timestamp = NOW()', [userid, bannedby]);
+    Log(color("EVENT BOOT: ", "red") + name + ' (UID ' + userid + ') has been banned by ' + bannedby + '.');
+    bot.speak(name + ' (UID ' + userid + ') has been banned by ' + bannedby + '.');
+    bot.boot(userid, 'You have been banned by ' + bannedby + '.');
 }
