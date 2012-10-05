8f4fc6ed5139e7c89fed69a2e7df4108a104c7ef
Mike Wills <mike@mikewills.me>
2012-10-04 20:49:02 -0500

    Added command `!realcount`

---
+++ commands/realcount.js
@@ -0,0 +1,22 @@
+exports.name = '!realcount';
+exports.hidden = true;
+exports.enabled = true;
+exports.matchStart = true;
+exports.handler = function(data, userid, source) {
+	GetValue("isModerating", 0, function(isModerating) {
+		if (isModerating === "true") {
+			var count = data.text.substring(11).split("-");
+			if (count.length !== 5) {
+				Speak("Syntax is !realcount #-#-#-#-# (Please use 'x' for empty seats.)", "", source, userid);
+			}
+			var x = 0;
+			for (var i in Djs){
+				if (count[x] !== "x"){
+					Djs[i].remainingPlays = Number(count[x]);
+				}
+				x++;
+			}
+			SpeakPlayCount();
+		}
+	});
+}
