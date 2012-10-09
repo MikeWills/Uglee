70591336f51a2191aee921719e324bb5bf750e13
Mike Wills <mike@mikewills.me>
2012-10-08 21:12:16 -0500

    New commands

---
+++ commands/newsong.js
@@ -0,0 +1,14 @@
+exports.name = '@' + botName.toLowerCase() + " pick a new song";
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data, userid, source) {
+	bot.playlistAll(function(oldPlaylist) {
+		bot.playlistReorder(0, oldPlaylist.list.length, function() {
+			bot.playlistAll(function(playlist) {
+				var nextSong = "I now have " + playlist.list[0].metadata.song + " by " + playlist.list[0].metadata.artist + " next.";
+				Speak(nextSong, "", source, userid);
+			});
+		});
+	});
+}
