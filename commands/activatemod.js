ece4d71b19476a62f579285cfc7d58330674904c
Mike Wills <mike@mikewills.me>
2012-10-09 13:52:08 -0500

    refactored

--- commands/activatemod.js
+++ commands/activatemod.js
@@ -3,12 +3,12 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
-		IsMod(userid, function(isMod) {
-			if (isMod) {
-				SetValue("isModerating", "true");
-				SetValue("autodj", "true");
-				SetValue("autobop", "true");
-				Speak("I am now moderating this room!")
-			}
-		});
+	IsMod(userid, function(isMod) {
+		if(isMod) {
+			SetValue("isModerating", "true");
+			SetValue("autodj", "true");
+			SetValue("autobop", "true");
+			Speak("I am now moderating this room!")
+		}
+	});
 }
--- commands/deactivatemod.js
+++ commands/deactivatemod.js
@@ -3,12 +3,12 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
-		IsMod(userid, function(isMod) {
-			if (isMod) {
-				SetValue("isModerating", "false");
-				SetValue("autodj", "false");
-				SetValue("autobop", "false");
-				Speak("I am no longer moderating this room!")
-			}
-		});
+	IsMod(userid, function(isMod) {
+		if(isMod) {
+			SetValue("isModerating", "false");
+			SetValue("autodj", "false");
+			SetValue("autobop", "false");
+			Speak("I am no longer moderating this room!")
+		}
+	});
 }
