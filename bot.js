ec5ffa532c913b3161d01d3d1410a41dc38638b3
Mike Wills <mike@mikewills.me>
2012-10-11 22:48:33 -0500

    Added the initial config file.

---
+++ bot.js
@@ -0,0 +1,20 @@
+// Bot details
+global.botAuthId = "auth+live+#######";
+global.botUserId = "######";
+global.botRoomId = "######";
+global.botName = "botName";
+global.botLaptop = "linux";
+global.botAdmins = ["#####", "######"]; // This can be 1 or more people
+
+// Database Details
+global.dbHost = "localhost";
+global.dbName = "tt_bots";
+global.dbTablePrefix = "bots_";
+global.dbLogin = "username";
+global.dbPassword = "password";
+
+// Flags
+global.logtoconsole = true;
+
+//Configuration
+require("./main.js");
