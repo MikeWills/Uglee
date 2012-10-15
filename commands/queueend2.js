f7e2ddec4af24ea3ee0c7010c86573203e72bf56
Mike Wills <mike@mikewills.me>
2012-10-14 22:36:07 -0500

    Forgot to make mod only.

--- commands/queueend2.js
+++ commands/queueend2.js
@@ -3,6 +3,10 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
-	SetValue("enableQueue", "false");
-	Speak("We are no longer using a queue.")
+	IsMod(userid, function(isMod) {
+		if(isMod) {
+			SetValue("enableQueue", "false");
+			Speak("We are no longer using a queue.");
+		}
+	});
 }
--- commands/queuestart1.js
+++ commands/queuestart1.js
@@ -3,6 +3,10 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
-	SetValue("enableQueue", "true");
-	Speak("We are now using a queue. Please type !q+ to add yourself to the list.")
+	IsMod(userid, function(isMod) {
+		if(isMod) {
+			SetValue("enableQueue", "true");
+			Speak("We are now using a queue. Please type !q+ to add yourself to the list.");
+		}
+	});
 }
