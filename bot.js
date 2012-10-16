ed14bf738aad805b483ef36d4d53f0c0bf0357b7
Mike Wills <mike@mikewills.me>
2012-10-15 23:47:57 -0500

    Added bot config changes

--- bot.js
+++ bot.js
@@ -16,5 +16,13 @@ global.dbPassword = "password";
 // Flags
 global.logtoconsole = true;
 
+// Configure sayings
+global.stepUpText = "Imma gonna help you out here.";
+global.stepDownText = "Looks like me no longer needed.";
+global.summonText = "Can you want to join us in All Music Mix (tt.fm/all_music_mix)?";
+global.welcomeText = "Welcome to the room @{u}! Be sure to familiarize yourself with the !rules.";
+global.ruleText = "[1] No more dubstep. [2] I will tell you how many songs you can play for each set (1-4) [3] Please wait the number DJ(s) I tell you between sets (I will suspend the wait if seats stay open). [4] While DJing hit awesome for the other DJs to show your support. [5] Be nice.";
+// TODO
+
 //Configuration
 require("./main.js");
