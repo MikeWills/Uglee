aa9f6b8fdf535569f1f196294fc9b5e0ec4f0d1d
Mike Wills <mike@mikewills.me>
2012-10-23 21:14:03 -0500

    Missing a function for the queue

--- functions.js
+++ functions.js
@@ -471,3 +471,31 @@ global.QueueStatus = function() {
 		}
 	});
 };
+
+global.dateDiff = function(a, b, format) {
+    var milliseconds = a - b;
+    var minutes = milliseconds / 60000;
+    var days = milliseconds / 86400000;
+    var hours = milliseconds / 3600000;
+    var weeks = milliseconds / 604800000;
+    var months = milliseconds / 2628000000;
+    var years = milliseconds / 31557600000;
+    if (format == "min") {
+        return Math.round(minutes);
+    }
+    if (format == "h") {
+        return Math.round(hours);
+    }
+    if (format == "d") {
+        return Math.round(days);
+    }
+    if (format == "w") {
+        return Math.round(weeks);
+    }
+    if (format == "m") {
+        return Math.round(months);
+    }
+    if (format == "y") {
+        return Math.round(years);
+    }
+};
