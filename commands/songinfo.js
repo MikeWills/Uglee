70bb4d30b229fd3fd0f26481155ac3d61c888364
Mike Wills <mike@mikewills.me>
2012-11-14 19:15:45 -0600

    Added command

---
+++ commands/songinfo.js
@@ -0,0 +1,8 @@
+exports.name = '/songinfo';
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data, userid, source) {
+	var endsongresponse = currentsong.song + ' stats: :+1: ' + currentsong.up + ' :-1: ' + currentsong.down + ' <3 ' + currentsong.snags;
+	Speak(endsongresponse);
+}
