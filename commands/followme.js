a135c47cd246f51c23160794cfa8d9fd40dedc37
Mike Wills <mike@mikewills.me>
2012-11-14 23:28:57 -0600

    Hopefull fix followme so that is works when not a fan.

--- commands/followme.js
+++ commands/followme.js
@@ -1,9 +1,10 @@
-exports.name = 'followme';
+exports.name = '/followme';
 exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
 	if (userid === botAdmin) {
+		bot.becomeFan(userid);
 		bot.stalk(botAdmin, function(stalkData) {
 			bot.roomDeregister();
 			bot.roomRegister(stalkData.roomId);
