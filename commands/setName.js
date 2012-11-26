5e5b84449342c99c3788f614f71280712bdd2d36
Mike Wills <mike@mikewills.me>
2012-11-26 00:04:17 -0600

    Added ability to change the bot's name (test)

---
+++ commands/setName.js
@@ -0,0 +1,10 @@
+exports.name = 'name';
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = true;
+exports.handler = function(data, userid, source) {
+	if (IsAdmin(userid) && source === 'pm') {
+		var name = data.text.substring(5);
+		bot.speak(name);
+	}
+}
