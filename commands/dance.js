7481157f2016e3804f0fe8e0a9e90d1a486c52fa
Mike Wills <mike@mikewills.me>
2012-12-17 22:13:12 -0600

    Changed the dance vote.

--- commands/dance.js
+++ commands/dance.js
@@ -3,13 +3,21 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data, userid, source) {
-	if (!alreadyVoted) {
-		if (danceRequesters.indexOf(userid) === -1) {
-			danceCount++;
-			danceRequesters.push(userid);
-			if (danceCount === 2) {
-				AwesomeSong();
-				alreadyVoted = true;
+	if(!alreadyVoted) {
+		if(currentDj !== userid) {
+			if(danceRequesters.indexOf(userid) === -1) {
+				danceCount++;
+				danceRequesters.push(userid);
+				bot.roomInfo(false, function(data) {
+					var neededDances = Math.round((data.room.metadata.listeners / 10) + 1);
+					if(neededDances < 2) { // Minium of 2
+						neededDances = 2;
+					}
+					if(danceCount === neededDances) {
+						AwesomeSong();
+						alreadyVoted = true;
+					}
+				});
 			}
 		}
 	} else {
