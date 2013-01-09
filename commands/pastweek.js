87224ac2409c88253df2482f397ed503e182ceff
Mike Wills <mike@mikewills.me>
2013-01-08 22:13:21 -0600

    Fixed /pastweek bug

--- commands/pastweek.js
+++ commands/pastweek.js
@@ -4,7 +4,11 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
-    client.query('SELECT username, upvotes FROM (SELECT djid, sum(up) as upvotes ' + 'FROM ' + dbName + '.' + dbTablePrefix + 'Song  WHERE `roomid` = ? AND started > DATE_SUB(NOW(), INTERVAL ' + '1 WEEK) GROUP BY djid) a INNER JOIN (SELECT * FROM (SELECT * FROM ' + dbName + '.' + dbTablePrefix + 'User  WHERE `roomid` = ? ORDER BY lastseen DESC) as test GROUP BY userid) b ON a.djid = b.userid' + ' ORDER BY upvotes DESC LIMIT 5', [currentRoomId, currentRoomId], function select(error, results, fields) {
+    client.query('SELECT username, upvotes FROM (SELECT djid, sum(up) as upvotes ' + 'FROM ' + dbName + '.' + dbTablePrefix + 
+    	'Song  WHERE `roomid` = ? AND started > DATE_SUB(NOW(), INTERVAL ' + 
+    	'1 WEEK) GROUP BY djid) a INNER JOIN (SELECT * FROM (SELECT * FROM ' + dbName + '.' + dbTablePrefix + 
+    	'User ORDER BY lastseen DESC) as test GROUP BY userid) b ON a.djid = b.userid' + ' ORDER BY upvotes DESC LIMIT 5', 
+    	[currentRoomId], function select(error, results, fields) {
         var response = 'DJs with the most points in the last week: Pun Masta Mike: 10,000,  ';
         for (i in results) {
             response += results[i]['username'] + ': ' + results[i]['upvotes'] + '▲. ';
