92f9c6f56dbe30b774200626ad24760114a9b391
Mike Wills <mike@mikewills.me>
2013-01-08 23:05:01 -0600

    Now only only commands when moderating

--- commands/rules.js
+++ commands/rules.js
@@ -3,5 +3,9 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
-    Speak(ruleText);
+	GetValue("isModerating", 0, function(isModerating) {
+		if(isModerating === "true") {
+			Speak(ruleText);
+		}
+	});
 }
