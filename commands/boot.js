ed1f45fd8876110b56ad16387bfcabc4b6a01fce
Mike Wills <mike@mikewills.me>
2013-01-15 23:16:29 -0600

    Added new command

---
+++ commands/boot.js
@@ -0,0 +1,19 @@
+exports.name = '/boot';
+exports.hidden = true;
+exports.enabled = true;
+exports.matchStart = true;
+exports.handler = function(data, userid, source) {
+    IsMod(userid, function(isMod) {
+        if (isMod) {
+            //Get name and userid
+            var givenname = data.text.substring(6);
+            client.query('SELECT userid FROM (SELECT * FROM ' + dbName + '.' + dbTablePrefix + 'User WHERE username LIKE ?) a ORDER BY lastseen DESC', [givenname], function select(error, results, fields) {
+                if (results != null && results.length > 0) {
+                    Log(color("EVENT BOOT: ", "red") + givenname + ' (UID ' + results[0]['userid'] + ') has been booted by ' + data.name + '.');
+                    bot.speak(givenname + ' (UID ' + results[0]['userid'] + ') has been booted by ' + data.name + '.');
+                    bot.boot(userid, 'When you can be good, you can come back.');
+                }
+            });
+        }
+    });
+}
