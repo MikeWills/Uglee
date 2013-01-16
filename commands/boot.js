0ba502e85bd48818ec37572bb8cf58edaa422cc3
Mike Wills <mike@mikewills.me>
2013-01-15 23:27:10 -0600

    Cleaned up command

--- commands/boot.js
+++ commands/boot.js
@@ -3,12 +3,10 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data, userid, source) {
-
     IsMod(userid, function(isMod) {
         if (isMod) {
             //Get name and userid
             var givenname = data.text.substring(7);
-            Log(givenname);
             client.query('SELECT userid FROM (SELECT * FROM ' + dbName + '.' + dbTablePrefix + 'User WHERE username LIKE ?) a ORDER BY lastseen DESC', [givenname], function select(error, results, fields) {
                 if (results != null && results.length > 0) {
                     Log(color("EVENT BOOT: ", "red") + givenname + ' (UID ' + results[0]['userid'] + ') has been booted by ' + data.name + '.', "error");
