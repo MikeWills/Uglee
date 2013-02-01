7a45768bdedd33dc16165031a2973abb52f4ccbb
Mike Wills <mike@mikewills.me>
2013-02-01 15:03:46 -0600

    This command is a work in progress

--- commands/getsonginfo.js
+++ commands/getsonginfo.js
@@ -1,7 +1,33 @@
-exports.name = '/getsonginfo';
+exports.name = '/aboutsong';
 exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data, userid, source) {
-
+  if (lastfmAPI != null){
+    var query = data.text.substring(10);
+    var components = query.split('-');
+    if (components.length !== 2){
+      lastfm.getSessionKey(function(result) {
+        console.log("session key = " + result.session_key);
+        if(result.success) {
+          lastfm.getTrackInfo({
+            track: currentsong.song, 
+            artist: currentsong.artist, 
+            callback: function(result) {
+              console.log("in callback, finished: ", result);
+            }
+          });
+    } else {
+      lastfm.getSessionKey(function(result) {
+        console.log("session key = " + result.session_key);
+        if(result.success) {
+          lastfm.getTrackInfo({
+            track: components[1], 
+            artist: components[0], 
+            callback: function(result) {
+              console.log("in callback, finished: ", result);
+            }
+          });
+    }
+  }
 }
