bf344148029275945334fa28ab9528e6399b1333
Mike Wills <mike@mikewills.me>
2013-02-06 23:31:45 -0600

    Fix bug in /bestdj and /worstdj

--- commands/bestdjs.js
+++ commands/bestdjs.js
@@ -6,8 +6,8 @@ exports.matchStart = false;
 exports.handler = function(data, userid, source) {
     client.query('SELECT username, upvotes FROM (SELECT djid, sum(up) AS upvotes ' + 'FROM ' + dbName + '.' + dbTablePrefix + 
     			 'Song WHERE `roomid` = ? GROUP BY djid ORDER BY sum(up) DESC LIMIT 3) a INNER JOIN (SELECT * FROM (SELECT * FROM ' + 
-    			 dbName + '.' + dbTablePrefix + 'User WHERE `roomid` = ? ORDER BY lastseen DESC) as test GROUP BY userid)' + 
-    			 ' b ON a.djid = b.userid ORDER BY upvotes DESC LIMIT 3', [currentRoomId, currentRoomId] , function select(error, results, fields) {
+    			 dbName + '.' + dbTablePrefix + 'User ORDER BY lastseen DESC) as test GROUP BY userid)' + 
+    			 ' b ON a.djid = b.userid ORDER BY upvotes DESC LIMIT 5', [currentRoomId] , function select(error, results, fields) {
         var response = 'The DJs with the most points accrued in this room: ';
         for (i in results) {
             response += results[i]['username'] + ': ' + results[i]['upvotes'] + ' points.  ';
--- commands/worstdjs.js
+++ commands/worstdjs.js
@@ -3,7 +3,10 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
-    client.query('SELECT username, downvotes FROM (SELECT djid, sum(down) AS downvotes ' + 'FROM ' + dbName + '.' + dbTablePrefix + 'Song WHERE `roomid` = ? GROUP BY djid ORDER BY sum(down) DESC LIMIT 3) a INNER JOIN (SELECT * FROM (SELECT * FROM ' + dbName + '.' + dbTablePrefix + 'User WHERE `roomid` = ? ORDER BY lastseen DESC) as test GROUP BY userid)' + ' b ON a.djid = b.userid ORDER BY downvotes DESC LIMIT 3',[currentRoomId, currentRoomId], function select(error, results, fields) {
+    client.query('SELECT username, downvotes FROM (SELECT djid, sum(down) AS downvotes ' + 'FROM ' + dbName + '.' + dbTablePrefix + 
+    	'Song WHERE `roomid` = ? GROUP BY djid ORDER BY sum(down) DESC LIMIT 3) a INNER JOIN (SELECT * FROM (SELECT * FROM ' + 
+    		dbName + '.' + dbTablePrefix + 'User ORDER BY lastseen DESC) as test GROUP BY userid)' + 
+    ' b ON a.djid = b.userid ORDER BY downvotes DESC LIMIT 3',[currentRoomId], function select(error, results, fields) {
         var response = 'The DJs with the most lames accrued in this room: ';
         for (i in results) {
             response += results[i]['username'] + ': ' + results[i]['downvotes'] + ' lames.  ';
