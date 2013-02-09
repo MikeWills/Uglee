a90b98d439d10e114c82d6b5d084e675e44d9f06
Mike Wills <mike@mikewills.me>
2013-02-09 16:08:50 -0600

    New command

---
+++ commands/d.js
@@ -0,0 +1,9 @@
+exports.name = 'd';
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data, userid, source) {
+	if (AllUsers[userid].laptop === "android" || AllUsers[userid].laptop === "iphone"){
+		Speak(AllUsers[userid].name + " is on a mobile device and it driving. Please don't distract them right now.");
+	}
+}
