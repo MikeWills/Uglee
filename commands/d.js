91dadb34f61f98bc364371436a7a0a8e776da586
Mike Wills <mike@mikewills.me>
2013-02-09 16:10:50 -0600

    Fix grammar

--- commands/d.js
+++ commands/d.js
@@ -4,6 +4,6 @@ exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
 	if (AllUsers[userid].laptop === "android" || AllUsers[userid].laptop === "iphone"){
-		Speak(AllUsers[userid].name + " is on a mobile device and it driving. Please don't distract them right now.");
+		Speak(AllUsers[userid].name + " is on a mobile device and is probably driving. Please don't distract them right now.");
 	}
 }
