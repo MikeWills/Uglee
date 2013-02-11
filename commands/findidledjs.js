2a5be551e29a747ada340ab3e3cfad1c43db35f7
Mike Wills <mike@mikewills.me>
2013-02-10 20:09:34 -0600

    Cleaned up

--- commands/findidledjs.js
+++ commands/findidledjs.js
@@ -3,17 +3,17 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
-	if (source === "pm") {
+	if(source === "pm") {
 		IsMod(userid, function(isMod) {
-			if (isMod) {
-					var pmText = "The following djs have been idle for (in minutes): ";
-					for (var z in Djs) {
-						var startDate = new Date();
-						var idleTime = Math.round((startDate - AllUsers[z].lastActivity) / 60000); // in minutes
-							pmText += AllUsers[z].name + ": " + idleTime + " on " + AllUsers[z].laptop + " | ";
-					}
+			if(isMod) {
+				var pmText = "The following djs have been idle for (in minutes): ";
+				for(var z in Djs) {
+					var startDate = new Date();
+					var idleTime = Math.round((startDate - AllUsers[z].lastActivity) / 60000); // in minutes
+					pmText += AllUsers[z].name + ": " + idleTime + " on " + AllUsers[z].laptop + " | ";
+				}
 
-					Speak(pmText, "", source, userid);
+				Speak(pmText, "", source, userid);
 			}
 		});
 	}
