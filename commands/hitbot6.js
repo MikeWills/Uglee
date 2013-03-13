0dff795705a2945ad3e3cad8800b83aa9a722e1e
Mike Wills <mike@mikewills.me>
2013-03-12 20:27:12 -0500

    Added a new command ... cause it's funny.

---
+++ commands/hitbot6.js
@@ -0,0 +1,13 @@
+exports.name = '/me fondles @' + botName.toLowerCase();
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data, userid, source) {
+	IsMod(userid, function(isMod) {
+		if(isMod) {
+			SpeakRandom(hitBot, AllUsers[userid].name);
+			bot.vote('up');
+			alreadyVoted = true;
+		}
+	});
+}
