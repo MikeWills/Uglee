d93110aafd296f4d523f56a238cd0345bdb58f57
Mike Wills <mike@mikewills.me>
2013-03-14 22:25:03 -0500

    Removed logging

--- commands/skip.js
+++ commands/skip.js
@@ -3,7 +3,6 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
-	Log("/skipsong");
 	var text = "@{u} PLEASE SKIP! You either have silence at the end of your song or a hung song.";
 	Speak(text, AllUsers[currentDj].name);
 	Speak(text, AllUsers[currentDj].name, "pm", currentDj);
