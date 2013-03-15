01271121171848d079663db8eb1640e27bd27029
Mike Wills <mike@mikewills.me>
2013-03-14 20:47:34 -0500

    Rmoved logging

--- commands/bop2.js
+++ commands/bop2.js
@@ -3,14 +3,11 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
-	Log("/bop");
 	var text = "";
 	for (var i in Djs) {
-		Log(i);
 		if (i !== currentDj) {
 			if (votedDjs.indexOf(i) == -1){
 				text += "@" + AllUsers[i].name + " ";
-				Log(text);
 			}
 		}
 	}
