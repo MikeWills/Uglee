0cccaf42af57eea1ccca41082f6132f53ea72b9e
Mike Wills <mike@mikewills.me>
2013-03-14 22:23:26 -0500

    Changed command to /skipsong

--- commands/skip.js
+++ commands/skip.js
@@ -1,9 +1,9 @@
-exports.name = '/skip';
+exports.name = '/skipsong';
 exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
-	Log("/skip");
+	Log("/skipsong");
 	var text = "@{u} PLEASE SKIP! You either have silence at the end of your song or a hung song.";
 	Speak(text, AllUsers[currentDj].name);
 	Speak(text, AllUsers[currentDj].name, "pm", currentDj);
