72542c1973dcff71de31ef44b5ea551d939752a9
Mike Wills <mike@mikewills.me>
2013-03-15 10:49:48 -0500

    Update ammmod.js

--- commands/ammmod.js
+++ commands/ammmod.js
@@ -1,27 +1,29 @@
 exports.name = '/ammmod';
 exports.hidden = true;
 exports.enabled = true;
-exports.matchStart = false;
+exports.matchStart = true;
 exports.handler = function(data, userid, source) {
-  if (currentRoomId  === "4ea390ac14169c0cc3caa078"){
-  IsMod(userid, function(isMod) {
-		if(isMod) {
+  if (currentRoomId === "4ea390ac14169c0cc3caa078") {
+    IsMod(userid, function(isMod) {
+      if (isMod) {
 
-      var setVal = "";
+        var setVal = "";
 
-      if (data.text.substring(7) == "on"){
-        setVal = "true";
-  		  Speak("I am now moderating this room!")
-      } else if (data.text.substring(8) == "off"){
-        setVal = "false";
-    	  Speak("I am no longer moderating this room!")
-      } else { return; }
+        if (data.text.substring(8) == "on") {
+          setVal = "true";
+          Speak("I am now moderating this room!")
+        } else if (data.text.substring(8) == "off") {
+          setVal = "false";
+          Speak("I am no longer moderating this room!")
+        } else {
+          return;
+        }
 
-			SetValue("isModerating", setVal);
-      SetValue("monitorHungSong", setVal);
-      SetValue("monitorAfk", setVal);
-      SetValue("autobop", setVal);
-		}
-	});
+        SetValue("isModerating", setVal);
+        SetValue("monitorHungSong", setVal);
+        SetValue("monitorAfk", setVal);
+        SetValue("autobop", setVal);
+      }
+    });
   }
 }
