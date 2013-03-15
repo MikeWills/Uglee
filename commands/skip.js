26ef799be10cf47f1cc70f7df13c251d5754e499
Mike Wills <mike@mikewills.me>
2013-03-14 22:20:43 -0500

    f

--- commands/skip.js
+++ commands/skip.js
@@ -3,6 +3,7 @@ exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data, userid, source) {
+	Log("/skip");
 	var text = "@{u} PLEASE SKIP! You either have silence at the end of your song or a hung song.";
 	Speak(text, AllUsers[currentDj].name);
 	Speak(text, AllUsers[currentDj].name, "pm", currentDj);
