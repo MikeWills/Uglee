27158e2ff76ad93324f1ecd35f4b0b9bbdf79f04
Mike Wills <mike@mikewills.me>
2013-03-17 20:34:19 -0500

    Removed the ability to reserve in All Music Mix

--- commands/reservefor.js
+++ commands/reservefor.js
@@ -3,15 +3,17 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data, userid, source) {
-	IsMod(userid, function(isMod) {
-		if(isMod) {
-			for(var i in AllUsers){
-				if (AllUsers[i].name.toLowerCase() == data.text.substring(13).toLowerCase()){
-					reserveredFor = i;
-					Speak("I am now saving the next spot for " + data.text.substring(12) + ".");
-					return;
+	if (currentRoomId !== "4ea390ac14169c0cc3caa078") {
+		IsMod(userid, function(isMod) {
+			if (isMod) {
+				for (var i in AllUsers) {
+					if (AllUsers[i].name.toLowerCase() == data.text.substring(13).toLowerCase()) {
+						reserveredFor = i;
+						Speak("I am now saving the next spot for " + data.text.substring(12) + ".");
+						return;
+					}
 				}
 			}
-		}
-	});
+		});
+	}
 }
--- commands/reservefor2.js
+++ commands/reservefor2.js
@@ -3,15 +3,17 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data, userid, source) {
-	IsMod(userid, function(isMod) {
-		if(isMod) {
-			for(var i in AllUsers){
-				if (AllUsers[i].name.toLowerCase() == data.text.substring(10).toLowerCase()){
-					reserveredFor = i;
-					Speak("I am now saving the next spot for " + data.text.substring(9) + ".");
-					return;
+	if (currentRoomId !== "4ea390ac14169c0cc3caa078") {
+		IsMod(userid, function(isMod) {
+			if (isMod) {
+				for (var i in AllUsers) {
+					if (AllUsers[i].name.toLowerCase() == data.text.substring(10).toLowerCase()) {
+						reserveredFor = i;
+						Speak("I am now saving the next spot for " + data.text.substring(9) + ".");
+						return;
+					}
 				}
 			}
-		}
-	});
+		});
+	}
 }
