3a4a42486b5e948927545bb89f41a4dda54f2845
Mike Wills <mike@mikewills.me>
2013-04-08 22:52:32 -0500

    put /help in the wrong location.

--- help.js
+++
@@ -1,8 +0,0 @@
-exports.name = '/helpme'; 
-exports.hidden = true;
-exports.enabled = true;
-exports.matchStart = false;
-exports.handler = function(data, userid, source) {
-	var text = "Complete list at: http://murl.me/uglee-help. Here are main ones: /count, @" + AllUsers[botUserId] +" stats, /dive, /rules.";
-	Speak(text, AllUsers[userid].name, source, userid);
-}
