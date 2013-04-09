14fefe4ce0d877b47507feabd2a1cbd4443b03e7
Mike Wills <mike@mikewills.me>
2013-04-09 11:20:59 -0500

    Create dance3.js

---
+++ commands/dance3.js
@@ -0,0 +1,26 @@
+exports.name = '.dance';
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = true;
+exports.handler = function(data, userid, source) {
+  if(!alreadyVoted) {
+		if(currentDj !== userid) {
+			if(danceRequesters.indexOf(userid) === -1) {
+				danceCount++;
+				danceRequesters.push(userid);
+				bot.roomInfo(false, function(data) {
+					var neededDances = Math.round((data.room.metadata.listeners / 10) + 1);
+					if(neededDances < 2) { // Minium of 2
+						neededDances = 2;
+					}
+					if(danceCount === neededDances) {
+						AwesomeSong();
+						alreadyVoted = true;
+					}
+				});
+			}
+		}
+	} else {
+		Speak("HEY! I'm boppin' here! What more you want???");
+	}
+}
