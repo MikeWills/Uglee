b685e4b04c8bba24c5ba2ab17dd0d7e8f0f2b753
Mike Wills <mike@mikewills.me>
2013-06-17 23:17:07 -0500

    Added clear queue command

---
+++ commands/queueclear.js
@@ -0,0 +1,12 @@
+exports.name = '/endq';
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data, userid, source) {
+	IsMod(userid, function(isMod) {
+		if(isMod) {
+			DjQueue = { "length": 0};
+			nextDj = null;
+		}
+	});
+}
