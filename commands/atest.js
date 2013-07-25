eb34612da34c9c56af24d12d54d914d69b31d70f
Mike Wills <mike@mikewills.me>
2013-07-24 21:46:02 -0500

    Adding a test command to troubleshoot a issue.

---
+++ commands/atest.js
@@ -0,0 +1,16 @@
+exports.name = ':fire:';
+exports.hidden = true;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data, userid, source) {
+	if (userid !== botUserId) {
+		console.log("Not bot");
+		IsMod(userid, function(isMod) {
+			if (isMod) {
+				console.log("Is Mod");
+				bot.vote('up');
+				alreadyVoted = true;
+			}
+		});
+	}
+}
