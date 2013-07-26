f619ef223111f4a683984fd168ba069fb78476e9
Mike Wills <mike@mikewills.me>
2013-07-26 13:13:24 -0500

    Create bopfu.js

---
+++ commands/bopfu.js
@@ -0,0 +1,8 @@
+exports.name = '/bop';
+exports.hidden = false;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data, userid, source) {
+	var text = "I don't work like that.";
+	Speak(text, userid, source, "");
+}
