e3ee520f37bf409d41e37fe93c5d40889f48f420
Mike Wills <mike@mikewills.me>
2013-08-05 21:03:04 -0500

    blah

--- commands/remmod.js
+++ commands/remmod.js
@@ -8,9 +8,9 @@ exports.handler = function(data, userid, source) {
 	if (userid !== botUserId) {
 		IsMod(userid, function(isMod) {
 			if (isMod) {
-				console.log("Removing " + data.text.substring(8) + " as a mod.");
-				bot.remModerator(data.text.substring(8));
-				Speak("Moderator id " + data.text.substring(8) + " removed.", "", source, userid);
+				console.log("Removing " + data.text.substring(9) + " as a mod.");
+				bot.remModerator(data.text.substring(9));
+				Speak("Moderator id " + data.text.substring(9) + " removed.", "", source, userid);
 			}
 		});
 	}
