7c5ad90f57eb44b06bb4d2aea5aec01798b46797
Mike Wills <mike@mikewills.me>
2013-08-12 22:28:14 -0500

    More fixes on mod changes

--- commands/addmod.js
+++ commands/addmod.js
@@ -3,14 +3,10 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data, userid, source) {
-	if (userid !== botUserId) {
-		IsAdmin(userid, function(isMod) {
-			if (isMod) {
-				botModed = true;
-				Log(userid + " ran moderator command.", "error", "New Moderator");
-				bot.addModerator(data.text.substring(8));
-				Speak("Moderator id " + data.text.substring(8) + " added.", "", source, userid);
-			}
-		});
+	if (IsAdmin(userid)) {
+		botModed = true;
+		Log(userid + " ran moderator command.", "error", "New Moderator");
+		bot.addModerator(data.text.substring(8));
+		Speak("Moderator id " + data.text.substring(8) + " added.", "", source, userid);
 	}
 }
--- commands/remmod.js
+++ commands/remmod.js
@@ -5,14 +5,10 @@ exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function(data, userid, source) {
-	if (userid !== botUserId) {
-		IsAdmin(userid, function(isMod) {
-			if (isMod) {
-				botModed = true;
-				Log(userid + " ran moderator command.", "error", "New Moderator");
-				bot.remModerator(data.text.substring(8));
-				Speak("Moderator id " + data.text.substring(8) + " removed.", "", source, userid);
-			}
-		});
+	if (IsAdmin(userid)) {
+		botModed = true;
+		Log(userid + " ran moderator command.", "error", "New Moderator");
+		bot.remModerator(data.text.substring(8));
+		Speak("Moderator id " + data.text.substring(8) + " removed.", "", source, userid);
 	}
 }
