aa9c9245413e23948c81f68fefb03304a5f82463
Mike Wills <mike@mikewills.me>
2013-08-12 21:22:45 -0500

    Fixed bug

--- commands/addmod.js
+++ commands/addmod.js
@@ -6,8 +6,8 @@ exports.handler = function(data, userid, source) {
 	if (userid !== botUserId) {
 		IsMod(userid, function(isMod) {
 			if (isMod) {
-				bot.remModerator(data.text.substring(9));
-				Speak("Moderator id " + data.text.substring(9) + " added.", "", source, userid);
+				bot.remModerator(data.text.substring(8));
+				Speak("Moderator id " + data.text.substring(8) + " added.", "", source, userid);
 			}
 		});
 	}
--- commands/remmod.js
+++ commands/remmod.js
@@ -8,8 +8,8 @@ exports.handler = function(data, userid, source) {
 	if (userid !== botUserId) {
 		IsMod(userid, function(isMod) {
 			if (isMod) {
-				bot.remModerator(data.text.substring(9));
-				Speak("Moderator id " + data.text.substring(9) + " removed.", "", source, userid);
+				bot.remModerator(data.text.substring(8));
+				Speak("Moderator id " + data.text.substring(8) + " removed.", "", source, userid);
 			}
 		});
 	}
